e680d8bd7bbfb61f80445f1763fef6a172806b3e
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-04-30 19:51:38 -0300

    js criado

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,13 +1,28 @@
 const email = document.getElementById('email')
+const bot = document.getElementById('bot')
+const ialert = document.getElementById('alert')
+const pes = document.getElementById('pescondido')
 
-email.addEventListener('focus', () => {
-    var botão = document.getElementById('bot')
-    botão.style.opacity = '0'
-    botão.style.display = 'none'
+bot.addEventListener('click', () => {//chama função anonima ao clicar
+    if (email.validity.typeMismatch) {//verifica se o formato esta incorreto e executa o bloco
+        event.preventDefault();//não envia o formulario //por padrão o formulario tenta enviar mesmo se estiver errado
+        window.alert('[ERROR] format incorrect')
+        email.value = ''
+        ialert.style.opacity = '1'
+        pes.style.opacity = '100%' // deixa o texto visivel
+        pes.style.userSelect = 'text' //permite selecionar o texto
+    }
+    
+})
+
+email.addEventListener('focus', () => {//chama função anonima ao ficar com focus 
+    bot.style.opacity = '0'//torna o icone invisivel ao ficar com foco o input
+    bot.style.display = 'none'
+    ialert.style.opacity = '0'
 })
 
 email.addEventListener('blur', () => {
-    var botão = document.getElementById('bot')
-    botão.style.opacity = '1'
-    botão.style.display = 'block'
+    bot.style.opacity = '1'//torna visivel o input quando fica fora de foco
+    bot.style.display = 'block'
 })
+
